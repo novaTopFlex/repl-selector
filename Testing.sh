79aaacf096d995213d47097d57d0932704982354
novaTopFlex <andrewkaymars@proton.me>
2024-05-31 14:38:28 -0400

    Update Testing.sh
    Updated to include support for alternate Python versions.

--- Testing.sh
+++ Testing.sh
@@ -1,14 +1,22 @@
-yad --center --info --text="Which version of Python? Select Python 3.13 \
-for the more stable beta release, or select Python 3.14 for the \
-more expreimental nightly release." \
---icon-name=python3.14 --button="Python 3.13":0 --button="Python 3.14":2 \
---title="Python" --timeout=15 --timeout-indicator=bottom \
---image=python3.14 --borders=6 --skip-taskbar
+yad --center --info --text="Select a Python version..." \
+--button="Python 3.12":0 --button="Python 3.11":1 --button="Python 3.10":2 --button="Python 3.9":3 --button="Python 3.13":4 --button="Python 3.14":5 --button="Python 2.7":6 \
+--title="Python" --timeout=30 --timeout-indicator=bottom \
+--image=python3 --borders=6 --skip-taskbar
 RETURN=$?
 if [[ $RETURN -eq 0 ]]; then
-    idle-python3.13;
+    idle-python3.12;
+elif [[ $RETURN -eq 1 ]]; then
+    idle-python3.11;
 elif [[ $RETURN -eq 2 ]]; then
+    idle-python3.10;
+elif [[ $RETURN -eq 3 ]]; then
+    idle-python3.9;
+elif [[ $RETURN -eq 4 ]]; then
+    idle-python3.13;
+elif [[ $RETURN -eq 5 ]]; then
     idle-python3.14;
+elif [[ $RETURN -eq 6 ]]; then
+    idle-python2.7;
 else
     echo $RETURN
 fi
